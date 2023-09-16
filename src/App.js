d5bc9cb4a4e285eea4d5c4d8ccf9a055f16f4140
Gauravk365kansal <132549852+Gauravk365kansal@users.noreply.github.com>
2023-09-16 06:58:13 -0400

    change into function component

--- src/App.js
+++ src/App.js
@@ -29,14 +29,14 @@ setProgress = (progress) => {
         <LoadingBar height={3} color='#f11946' progress={this.state.progress} 
         /> 
         <Routes>
-        <Route exact path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize = {this.pageSize} country = "in" Category = "general"></News>}/>
-        <Route exact path="/home" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize = {this.pageSize} country = "in" Category = "general"></News>}/>
-        <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize = {this.pageSize} country = "in" Category = "business"></News>}/>
+        <Route exact path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize = {this.pageSize} country = "in" category = "general"></News>}/>
+        <Route exact path="/home" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize = {this.pageSize} country = "in" category = "general"></News>}/>
+        <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize = {this.pageSize} country = "in" category = "business"></News>}/>
         <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize = {this.pageSize} country = "in" Category = "entertainment"></News>}/>
-        <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize = {this.pageSize} country = "in" Category = "health"></News>}/>
-        <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize = {this.pageSize} country = "in" Category = "science"></News>}/>
-        <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize = {this.pageSize} country = "in" Category = "technology"></News>}/>
-        <Route exact path="/sports" element={<News setProgress={this.setProgress}apiKey={this.apiKey} key="sports" pageSize = {this.pageSize} country = "in" Category = "sports"></News>}/>
+        <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize = {this.pageSize} country = "in" category = "health"></News>}/>
+        <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize = {this.pageSize} country = "in" category = "science"></News>}/>
+        <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize = {this.pageSize} country = "in" category = "technology"></News>}/>
+        <Route exact path="/sports" element={<News setProgress={this.setProgress}apiKey={this.apiKey} key="sports" pageSize = {this.pageSize} country = "in" category = "sports"></News>}/>
         </Routes>
         </Router>
       </div>
